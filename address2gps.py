431836ce9949a35963c389224a087c9dff34b126
LuoZihYuan <fudan28024@gmail.com>
2017-06-01 22:17:33 +0800

    refactor: replace geocoder.py with geo package

--- address2gps.py
+++ address2gps.py
@@ -1,13 +1,16 @@
-
 # -*- coding: UTF-8 -*-
-""" Geocode address into GPS coordinates """
+"""
+    Geocode address into GPS coordinates
+"""
 
+# Python Standard Library
 import os
 import re
-import yaml
 import shutil
+# Third Party Library
+import geo
+import yaml
 import unicodecsv as csv
-from geocoder import Geocoder
 
 RESOURCES_PATH = "resources/"
 GEOCODE_HISTORY = "geocode_history.yaml"
@@ -15,7 +18,7 @@ GEOCODE_HISTORY = "geocode_history.yaml"
 try:
     YAMLSTREAM = open(GEOCODE_HISTORY, "r+")
 except IOError:
-    print "Unable to access critical file/directory"
+    print("Unable to access critical file/directory")
 HISTORY = yaml.load(YAMLSTREAM)
 def update_yaml():
     """ Update HISTORY back to yaml """
@@ -36,12 +39,12 @@ def categorize(file_name):
 def get_next_path():
     """ Get next path from history """
     path = ""
-    CATEG_PR = ["A", "B", "C"]
+    categ_pr = ["A", "B", "C"]
     # CITY_PR = ["A", "B", "E", "F", "H"]
     if HISTORY["active"]:
         path = HISTORY["active"]
     else:
-        for code in CATEG_PR:
+        for code in categ_pr:
             _cat = categorize(code)
             try:
                 path = HISTORY["queue"][_cat].pop(0)
@@ -51,7 +54,7 @@ def get_next_path():
                 continue
     return path
 
-print "Checking for newfiles..."
+print("Checking for newfiles...")
 SUBPATHS = []
 for folder in os.listdir(RESOURCES_PATH)[1:]:
     if not os.path.isdir(RESOURCES_PATH + folder):
@@ -66,8 +69,7 @@ for folder in os.listdir(RESOURCES_PATH)[1:]:
                 HISTORY["queue"][cat].append(subpath)
 update_yaml()
 
-print "Initiating Geocoder..."
-shared_geocoder = Geocoder()
+print("Initiating Geocoder...")
 def center_gps(raw_address):
     """ Gets the representitive GPS coordinates within the interval of raw_address """
     matches = re.findall(r"\d+~\d+", raw_address)
@@ -82,7 +84,7 @@ def center_gps(raw_address):
         longitude_total = 0.0
         for num in sample[:]:
             address = raw_address.replace(match, str(num))
-            coordinates = shared_geocoder.geocode(address)
+            coordinates = geo.geocode(address)
             if coordinates["lat"] and coordinates["lon"]:
                 lattitude_total += coordinates["lat"]
                 longitude_total += coordinates["lon"]
@@ -101,50 +103,49 @@ ADDRESS_FIELD = u"土地區段位置或建物區門牌"
 LATITUDE_FIELD = u"緯度"
 LONGITUDE_FIELD = u"經度"
 BANNED_KEYWORD = u"地號"
-GARBLED_LETTER = "&#"
 
-next_path = get_next_path()
-while next_path:
-    print "Geocoding " + next_path
-    with open(RESOURCES_PATH + next_path, "r") as csvinput:
-        HISTORY["active"] = next_path
+NEXT_PATH = get_next_path()
+while NEXT_PATH:
+    print("Geocoding " + NEXT_PATH)
+    with open(RESOURCES_PATH + NEXT_PATH, "r") as csvinput:
+        HISTORY["active"] = NEXT_PATH
         update_yaml()
-        reader = csv.DictReader(csvinput, encoding="big5", errors="ignore")
-        fieldnames = reader.fieldnames + [LATITUDE_FIELD, LONGITUDE_FIELD]
+        READER = csv.DictReader(csvinput, encoding="big5", errors="ignore")
+        FIELDNAMES = READER.fieldnames + [LATITUDE_FIELD, LONGITUDE_FIELD]
         with open(RESOURCES_PATH + TEMPORARY_FILE, "a") as createfile:
             pass
         with open(RESOURCES_PATH + TEMPORARY_FILE, "r+") as csvoutput:
-            skip_reader = csv.DictReader(csvoutput, encoding="big5", errors="ignore")
-            writer = csv.DictWriter(csvoutput, fieldnames, encoding="big5", errors="ignore")
-            previous_address = ""
-            previous_gps = {"lat": None, "lon": None}
-            if not skip_reader.fieldnames:
-                writer.writeheader()
-            for row in skip_reader:
+            SKIP_READER = csv.DictReader(csvoutput, encoding="big5", errors="ignore")
+            WRITER = csv.DictWriter(csvoutput, FIELDNAMES, encoding="big5", errors="ignore")
+            PREVIOUS_ADDRESS = ""
+            PREVIOUS_GPS = {"lat": None, "lon": None}
+            if not SKIP_READER.fieldnames:
+                WRITER.writeheader()
+            for row in SKIP_READER:
                 previous_address = row[ADDRESS_FIELD]
-                previous_gps["lat"] = row[LATITUDE_FIELD]
-                previous_gps["lon"] = row[LONGITUDE_FIELD]
-                reader.next()
-            for row in reader:
+                PREVIOUS_GPS["lat"] = row[LATITUDE_FIELD]
+                PREVIOUS_GPS["lon"] = row[LONGITUDE_FIELD]
+                READER.next()
+            for row in READER:
                 rough_address = row[ADDRESS_FIELD]
-                if BANNED_KEYWORD in rough_address or GARBLED_LETTER in rough_address:
+                if BANNED_KEYWORD in rough_address:
                     pass
                 elif previous_address == rough_address:
-                    row[LATITUDE_FIELD] = previous_gps["lat"]
-                    row[LONGITUDE_FIELD] = previous_gps["lon"]
+                    row[LATITUDE_FIELD] = PREVIOUS_GPS["lat"]
+                    row[LONGITUDE_FIELD] = PREVIOUS_GPS["lon"]
                 elif previous_address != rough_address:
                     result = center_gps(rough_address)
                     row[LATITUDE_FIELD] = result["lat"]
                     row[LONGITUDE_FIELD] = result["lon"]
                     previous_address = rough_address
-                    previous_gps["lat"] = result["lat"]
-                    previous_gps["lon"] = result["lon"]
-                writer.writerow(row)
-    shutil.copyfile(RESOURCES_PATH + TEMPORARY_FILE, RESOURCES_PATH + next_path)
+                    PREVIOUS_GPS["lat"] = result["lat"]
+                    PREVIOUS_GPS["lon"] = result["lon"]
+                WRITER.writerow(row)
+    shutil.copyfile(RESOURCES_PATH + TEMPORARY_FILE, RESOURCES_PATH + NEXT_PATH)
     os.remove(RESOURCES_PATH + TEMPORARY_FILE)
-    HISTORY["done"][categorize(next_path)].append(next_path)
+    HISTORY["done"][categorize(NEXT_PATH)].append(NEXT_PATH)
     HISTORY["active"] = ""
     update_yaml()
-    next_path = get_next_path()
+    NEXT_PATH = get_next_path()
 
 YAMLSTREAM.close()
